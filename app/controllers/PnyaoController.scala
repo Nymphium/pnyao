fd4f773ac661faf6f1fa49185107a575f4e5fc0e
Kawahara Satoru <s1311350@gmail.com>
2018-06-05 02:52:51 +0900

    unify controller files related to Info into PnyaoController.scala

--- app/controllers/PnyaoController.scala
+++ app/controllers/PnyaoController.scala
@@ -1,9 +1,17 @@
 package controllers
 
-import scala.concurrent.Future
+import scala.concurrent.Future,
+scala.concurrent.ExecutionContext.Implicits.global
+
+import java.io.File, java.net.URLDecoder,
+java.nio.file.{Files => NIOFiles, Paths, StandardCopyOption}
+
+import com.github.nymphium.pnyao.Files
+import services.RenderPnyao
 
 import
     play.api.Logger
+  , play.api.libs.Files.SingletonTemporaryFileCreator
   , play.api.mvc._
   , play.api.libs.json._
   , play.api.libs.functional.syntax._
@@ -11,8 +19,9 @@ import
 import javax.inject._
 
 @Singleton
-class PnyaoController @Inject()(cc: ControllerComponents, pnyao: services.Pnyao) extends AbstractController(cc) {
+class PnyaoController @Inject()(cc: ControllerComponents, pnyao: services.Pnyao)(implicit assetsFinder: AssetsFinder) extends AbstractController(cc) {
   protected case class UpTup(`type`: String, idx: Int, parent: String, value: String)
+  protected case class C(value: String)
 
   implicit val reads = (
     (JsPath \ "type").read[String] and
@@ -21,7 +30,11 @@ class PnyaoController @Inject()(cc: ControllerComponents, pnyao: services.Pnyao)
     (JsPath \ "value").read[String]
   )(UpTup.apply(_, _, _, _))
 
-  def update = Action.async(parse.json) { request =>
+  def index = Action { implicit request =>
+    Ok(views.html.index(RenderPnyao.render(pnyao.getDB).toString))
+  }
+
+  def updateInfo = Action.async(parse.json) { request =>
     request.body
       .validate[UpTup]
       .fold(
@@ -34,5 +47,51 @@ class PnyaoController @Inject()(cc: ControllerComponents, pnyao: services.Pnyao)
               "message" -> UpTup(typ, idx, parent, value).toString)))
         }}
       )
+  }
+
+  def openPDF(href: String) = Action {
+    val it = URLDecoder.decode(href, "UTF-8")
+    val fileToServe = SingletonTemporaryFileCreator.create("pnyaotmp", ".pdf")
+    NIOFiles.copy(Paths.get(it),
+               fileToServe.path,
+               StandardCopyOption.REPLACE_EXISTING)
+    Logger.info(s"open ${it}")
+    Ok.sendFile(
+      content = fileToServe.path.toFile,
+      onClose = () => { fileToServe.delete }
+    )
+  }
+
+  def deleteEntry(path: String) = Action {
+    val dpath = URLDecoder.decode(path, "UTF-8")
+    val db = pnyao.getDB
+
+    if (db.exists { case (path, _) => path == dpath }) {
+      pnyao.deleteDBEntry(dpath)
+      Logger.info(s"delete directory ${dpath}")
+      Ok("deleted")
+    } else { BadRequest(s"There is no entry ${dpath}") }
+  }
+
+  def addEntry(path: String) = Action {
+    val dpath = URLDecoder.decode(path, "UTF-8")
+    val file = new File(dpath)
+
+    if (! file.exists || ! file.isDirectory) {
+      BadRequest(s"${dpath} is not exist");
+    } else {
+      val db = pnyao.getDB
+
+      if (db.exists {case (path, _) => path == dpath}) {
+        Ok(Json.obj("status" -> "alreadyloaded", "message" -> s"${dpath} is already loaded"))
+      } else {
+        val content = Files.traverseDirectory(dpath)
+
+        pnyao.addDB(dpath, content)
+
+        // TODO: response渡してからdbに書き込んだりしたい
+        Ok(Json.obj("status" -> "OK", "message" -> RenderPnyao.dirnInfoAsBody(dpath, content).toString))
+      }
+    }
   }
 }
